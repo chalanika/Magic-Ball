2dc6a75b2a4e335f54cc0150902462c5a1cafbbc
Chalanika <chalanikasewwandi@gmail.com>
2020-05-09 07:07:26 +0530

    created app bar

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor:Colors.blue,
+          appBar:AppBar(
+            title:Text('Ask Me Anything'),
+            backgroundColor:Colors.black,
+          ),
+        ),
+      ),
+    );
